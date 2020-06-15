87a4c0e768dbfd22d44c49825dda5fefe9aba4c1
Mauricio Vazquez <maurovz@gmail.com>
2020-06-15 21:14:48 +0200

    added more viewmodel variables

--- MarvelHeroes/ViewModels/HeroViewModel.swift
+++ MarvelHeroes/ViewModels/HeroViewModel.swift
@@ -6,5 +6,16 @@ struct HeroViewModel {
   var id: Int {
     return hero.id
   }
+  
+  var name: String {
+    return hero.name
+  }
+  
+  var description: String {
+    return hero.description
+  }
+  
+  var thumbnailImage: String {
+    return hero.thumbnail.path + hero.thumbnail.imageExtension
+  }
 }
-
