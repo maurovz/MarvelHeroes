86125fe4f199d3616d68015adf444daef085f11e
Mauricio Vazquez <maurovz@gmail.com>
2020-06-15 20:06:48 +0200

    created hero list view

--- MarvelHeroes/Delegates/SceneDelegate.swift
+++ MarvelHeroes/Delegates/SceneDelegate.swift
@@ -5,10 +5,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    let contentView = ContentView()
     if let windowScene = scene as? UIWindowScene {
       let window = UIWindow(windowScene: windowScene)
-      window.rootViewController = UIHostingController(rootView: contentView)
+      window.rootViewController = UIHostingController(rootView: HeroListView())
       self.window = window
       window.makeKeyAndVisible()
     }
--- MarvelHeroes/Views/HeroListView.swift
+++ MarvelHeroes/Views/HeroListView.swift
@@ -1,13 +1,17 @@
 import SwiftUI
 
-struct ContentView: View {
+struct HeroListView: View {
     var body: some View {
-        Text("Hello, World!")
+      NavigationView {
+        List {
+          Text("Hero's Name")
+        }
+      }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HeroListView()
     }
 }
