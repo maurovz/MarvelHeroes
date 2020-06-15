36cb56b721a3d9119f5477eb4abcafd3cd081639
Mauricio Vazquez <maurovz@gmail.com>
2020-06-15 18:01:54 +0200

    Initial Commit

---
+++ MarvelHeroes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelHeroes
+//
+//  Created by Mauricio Vazquez on 6/15/20.
+//  Copyright © 2020 MauVZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
