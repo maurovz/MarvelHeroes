0e81e59560e598c437d59ccc04ce46f0edaeb694
Mauricio Vazquez <maurovz@gmail.com>
2020-06-16 13:42:17 +0200

    added hero list webservice

---
+++ MarvelHeroes/App/Constants.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+
+struct Constants {
+  static let apiURL = "https://gateway.marvel.com:443/v1/public/"
+  static let heroesEndpoint = "characters?apikey=\(apiKey)&hash=\(hash)&ts=\(timeStamp)"
+  static let apiKey = "0c5a611113f2aa779e8988bb6a0e8447"
+  static let timeStamp = "ts2020"
+  static let hash = "cb0c0b92b46b5a48de738ca8d92470e6"
+}
---
+++ MarvelHeroes/Services/WebService.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+class WebService {
+  
+  func getHeroes(completion: @escaping (([Hero]?) -> Void)) {
+    guard let url = URL(string: Constants.apiURL + Constants.heroesEndpoint) else {
+      fatalError("Url is not correct")
+    }
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      guard let data = data, error == nil else {
+        DispatchQueue.main.async {
+          completion(nil)
+        }
+        return
+      }
+      do {
+        let heroes = try JSONDecoder().decode(APIResponse.self, from: data)
+        DispatchQueue.main.async {
+          completion(heroes.data.results)
+        }
+      } catch {
+        print(error)
+      }
+    }.resume()
+  }
+}
