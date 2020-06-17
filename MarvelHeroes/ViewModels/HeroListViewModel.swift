4316bb7e09e223ef62fb57d0a1021fb8504c9df7
Mauricio Vazquez <maurovz@gmail.com>
2020-06-17 22:58:16 +0200

    added httpclient that can be mocked

--- MarvelHeroes/ViewModels/HeroListViewModel.swift
+++ MarvelHeroes/ViewModels/HeroListViewModel.swift
@@ -2,13 +2,15 @@ import Foundation
 
 class HeroListViewModel: ObservableObject {
   @Published var heroes: [HeroViewModel] = [HeroViewModel]()
+  private let webService: WebService
 
   init() {
+    webService = WebService(httpClient: HttpClient(session: URLSession(configuration: .default)))
     fetchHeroes()
   }
 
   func fetchHeroes() {
-    WebService().getHeroes { heroes in
+    webService.getHeroes { heroes in
       guard let heroes = heroes else { return }
       self.heroes = heroes.map(HeroViewModel.init)
     }
