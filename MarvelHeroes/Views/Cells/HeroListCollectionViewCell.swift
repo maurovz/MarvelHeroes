83169a6a068860e949eb451d1427076b240b86bd
Mauricio Vazquez <maurovz@gmail.com>
2020-06-28 14:12:08 +0200

    added flow layout

--- MarvelHeroes/Views/Cells/HeroListCollectionViewCell.swift
+++ MarvelHeroes/Views/Cells/HeroListCollectionViewCell.swift
@@ -1,4 +1,18 @@
 import UIKit
 
 class HeroListCollectionViewCell: UICollectionViewCell {
+  @IBOutlet var imageView: UIImageView!
+  static let identifier = "HeroListCollectionViewCell"
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  public func configure(with image: UIImage) {
+    imageView.image = image
+  }
+  
+  static func nib() -> UINib {
+    return UINib(nibName: identifier, bundle: nil)
+  }
 }
--- MarvelHeroes/Views/HeroListViewController.swift
+++ MarvelHeroes/Views/HeroListViewController.swift
@@ -2,29 +2,41 @@ import UIKit
 
 class HeroListViewController: UIViewController {
   @IBOutlet weak var collectionView: UICollectionView!
-  private let reuseIdentifier = "HeroListCollectionViewCell"
-
+  private let screenSize = UIScreen.main.bounds
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupCollectionView()
   }
 }
 
 extension HeroListViewController: UICollectionViewDelegateFlowLayout {
+  func setupCollectionView() {
+    let layout = UICollectionViewFlowLayout()
+    layout.itemSize = CGSize(width: 120, height: 120 )
+    collectionView.collectionViewLayout = layout
+    collectionView.register(UINib(nibName: HeroListCollectionViewCell.identifier, bundle: .main), forCellWithReuseIdentifier: HeroListCollectionViewCell.identifier)
+    collectionView.delegate = self
+    collectionView.dataSource = self
+  }
+  
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
                       sizeForItemAt indexPath: IndexPath) -> CGSize {
-    return CGSize(width: 150, height: 150)
+    return CGSize(width: 120, height: 120)
   }
 }
 
 extension HeroListViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 2
+    return 10
   }
-
+  
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HeroListCollectionViewCell.identifier, for: indexPath) as! HeroListCollectionViewCell
+    cell.configure(with: UIImage(named: "hero")!)
+    
     return cell
   }
 }
 
-//extension HeroListViewController: UICollectionViewDelegate { }
+extension HeroListViewController: UICollectionViewDelegate { }
