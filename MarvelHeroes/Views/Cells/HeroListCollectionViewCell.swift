9e570f065718406d62d262fdfb8804be81e02edd
Mauricio Vazquez <maurovz@gmail.com>
2020-06-28 18:26:35 +0200

    added basic hero cell

--- MarvelHeroes/Views/Cells/HeroListCollectionViewCell.swift
+++ MarvelHeroes/Views/Cells/HeroListCollectionViewCell.swift
@@ -3,16 +3,30 @@ import UIKit
 class HeroListCollectionViewCell: UICollectionViewCell {
   @IBOutlet var imageView: UIImageView!
   static let identifier = "HeroListCollectionViewCell"
+  @IBOutlet var blurrView: UIVisualEffectView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var creationDateLabel: UILabel!
   
   override func awakeFromNib() {
     super.awakeFromNib()
+    createBlurrView()
+    createHeroTittle()
   }
+  
+  private func createBlurrView() {
+    let blurredView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+    blurredView.frame = self.blurrView.bounds
+    blurredView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    blurrView.addSubview(blurredView)
+    blurrView.sendSubviewToBack(blurredView)
+  }
+  
+  func createHeroTittle() {
+    
+  }
+  
   
   public func configure(with image: UIImage) {
     imageView.image = image
-  }
-  
-  static func nib() -> UINib {
-    return UINib(nibName: identifier, bundle: nil)
   }
 }
--- MarvelHeroes/Views/HeroListViewController.swift
+++ MarvelHeroes/Views/HeroListViewController.swift
@@ -13,16 +13,16 @@ class HeroListViewController: UIViewController {
 extension HeroListViewController: UICollectionViewDelegateFlowLayout {
   func setupCollectionView() {
     let layout = UICollectionViewFlowLayout()
-    layout.itemSize = CGSize(width: 120, height: 120 )
+    layout.itemSize = CGSize(width: screenSize.width / 2, height: screenSize.height / 3 )
     collectionView.collectionViewLayout = layout
     collectionView.register(UINib(nibName: HeroListCollectionViewCell.identifier, bundle: .main), forCellWithReuseIdentifier: HeroListCollectionViewCell.identifier)
     collectionView.delegate = self
     collectionView.dataSource = self
   }
-  
+
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
                       sizeForItemAt indexPath: IndexPath) -> CGSize {
-    return CGSize(width: 120, height: 120)
+    return CGSize(width: screenSize.width / 2, height: screenSize.height / 3)
   }
 }
 
@@ -30,11 +30,12 @@ extension HeroListViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return 10
   }
-  
+
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HeroListCollectionViewCell.identifier, for: indexPath) as! HeroListCollectionViewCell
     cell.configure(with: UIImage(named: "hero")!)
-    
+    cell.layer.cornerRadius = 10
+    cell.layer.masksToBounds = true
     return cell
   }
 }
