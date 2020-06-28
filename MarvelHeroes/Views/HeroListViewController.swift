4c23eba6ed4e7ad1e7cf968cf22af4e646727ff4
Mauricio Vazquez <maurovz@gmail.com>
2020-06-28 12:44:40 +0200

    added empty cell

--- MarvelHeroes/Views/HeroListViewController.swift
+++ MarvelHeroes/Views/HeroListViewController.swift
@@ -1,20 +1,30 @@
 import UIKit
 
 class HeroListViewController: UIViewController {
+  @IBOutlet weak var collectionView: UICollectionView!
+  private let reuseIdentifier = "HeroListCollectionViewCell"
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 }
 
+extension HeroListViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 150, height: 150)
+  }
+}
+
 extension HeroListViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
+    return 2
   }
 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeroListCollectionViewCell", for: indexPath)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     return cell
   }
 }
 
-extension HeroListViewController: UICollectionViewDelegate { }
+//extension HeroListViewController: UICollectionViewDelegate { }
