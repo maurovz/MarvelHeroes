d623dd0dc96e82afe70fdf90f424ec6a4f28dc30
Mauricio Vazquez <maurovz@gmail.com>
2020-06-29 17:00:28 +0200

    finished featured collectionview

--- MarvelHeroes/Views/Cells/FeaturedCollectionViewCell.swift
+++ MarvelHeroes/Views/Cells/FeaturedCollectionViewCell.swift
@@ -1,7 +1,14 @@
 import UIKit
 
 class FeaturedCollectionViewCell: UICollectionViewCell {
+  @IBOutlet weak var imageView: UIImageView!
+  static let identifier = "FeaturedCollectionViewCell"
+
   override func awakeFromNib() {
     super.awakeFromNib()
+  }
+
+  public func configure(with image: UIImage) {
+    imageView.image = image
   }
 }
--- MarvelHeroes/Views/HeroListViewController.swift
+++ MarvelHeroes/Views/HeroListViewController.swift
@@ -5,14 +5,16 @@ class HeroListViewController: UIViewController {
   @IBOutlet weak var featuredHeroesCollectionView: UICollectionView!
   @IBOutlet weak var heroListCollectionView: UICollectionView!
   private let screenSize = UIScreen.main.bounds
+  @IBOutlet weak var featuredPageControl: UIPageControl!
+//  let images = ["hero", "avatar"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setupCollectionView()
-    addLogo(image: UIImage(named: "logo"))
+    addNavigationBarLogo(image: UIImage(named: "logo"))
   }
 
-  func addLogo(image: UIImage?) {
+  private func addNavigationBarLogo(image: UIImage?) {
     let imageView = UIImageView(image: image)
     imageView.contentMode = .scaleAspectFit
     self.navigationItem.titleView = imageView
@@ -22,22 +24,48 @@ class HeroListViewController: UIViewController {
     super.traitCollectionDidChange(previousTraitCollection)
     guard previousTraitCollection != nil else { return }
     heroListCollectionView?.collectionViewLayout.invalidateLayout()
+    featuredHeroesCollectionView?.collectionViewLayout.invalidateLayout()
   }
 }
 
 extension HeroListViewController: UICollectionViewDelegateFlowLayout {
-  func setupCollectionView() {
+  private func setupCollectionView() {
+    setupLayouts()
+    registerCollectionViewCellsNibs()
+  }
+
+  private func setupLayouts() {
     let layout = UICollectionViewFlowLayout()
     layout.minimumInteritemSpacing = 5
     layout.minimumLineSpacing = 10
     heroListCollectionView.collectionViewLayout = layout
-    heroListCollectionView.register(UINib(nibName: HeroListCollectionViewCell.identifier,
-                                  bundle: .main), forCellWithReuseIdentifier: HeroListCollectionViewCell.identifier)
+    let featuredLayout = UICollectionViewFlowLayout()
+    featuredLayout.minimumInteritemSpacing = 0
+    featuredLayout.minimumLineSpacing = 0
+    featuredLayout.scrollDirection = .horizontal
+    featuredHeroesCollectionView.collectionViewLayout = featuredLayout
+  }
+
+  private func registerCollectionViewCellsNibs() {
+    heroListCollectionView.register(
+    UINib(nibName: HeroListCollectionViewCell.identifier, bundle: .main),
+    forCellWithReuseIdentifier: HeroListCollectionViewCell.identifier)
+    featuredHeroesCollectionView.register(
+    UINib(nibName: FeaturedCollectionViewCell.identifier, bundle: .main),
+    forCellWithReuseIdentifier: FeaturedCollectionViewCell.identifier)
   }
 
   func collectionView(_ collectionView: UICollectionView,
                       layout collectionViewLayout: UICollectionViewLayout,
                       sizeForItemAt indexPath: IndexPath) -> CGSize {
+    if collectionView == heroListCollectionView {
+      return getHeroCellSize(collectionView: collectionView)
+    } else {
+      return getFeaturedCellSize(collectionView: collectionView)
+    }
+  }
+
+  private func getHeroCellSize(collectionView: UICollectionView) -> CGSize {
     let numberOfColumns: CGFloat =  2
     let cellWidth = collectionView.frame.size.width
     let height = collectionView.frame.size.width / 2
@@ -45,30 +73,50 @@ extension HeroListViewController: UICollectionViewDelegateFlowLayout {
     let cellSpacing: CGFloat = 5
     return CGSize(width: (cellWidth / numberOfColumns) - (xInset + cellSpacing), height: height)
   }
+
+  private func getFeaturedCellSize(collectionView: UICollectionView) -> CGSize {
+    let cellWidth = collectionView.frame.size.width
+    let height = collectionView.frame.size.height
+    return CGSize(width: cellWidth, height: height)
+  }
 }
 
 extension HeroListViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
                       insetForSectionAt section: Int) -> UIEdgeInsets {
-    let sideMargins: CGFloat = 10
-     return UIEdgeInsets(top: 0, left: sideMargins, bottom: 0, right: sideMargins)
+    if collectionView == featuredHeroesCollectionView {
+      return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    } else {
+      let sideMargins: CGFloat = 10
+      return UIEdgeInsets(top: 0, left: sideMargins, bottom: 0, right: sideMargins)
+    }
   }
 
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     if collectionView == heroListCollectionView {
       return 10
     } else {
-      return 5
+      return 10
     }
   }
 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(
-      withReuseIdentifier: HeroListCollectionViewCell.identifier, for: indexPath) as? HeroListCollectionViewCell
-      else {
-        fatalError("Error dequeue Reusable Cell")
+    if collectionView == featuredHeroesCollectionView {
+      guard let cell = collectionView.dequeueReusableCell(
+        withReuseIdentifier: FeaturedCollectionViewCell.identifier, for: indexPath) as? FeaturedCollectionViewCell
+        else {
+          fatalError("Error dequeuing Reusable Cell")
+      }
+      cell.configure(with: UIImage(named: "comic")!)
+      return cell
+    } else {
+      guard let cell = collectionView.dequeueReusableCell(
+        withReuseIdentifier: HeroListCollectionViewCell.identifier, for: indexPath) as? HeroListCollectionViewCell
+        else {
+          fatalError("Error dequeuing Reusable Cell")
+      }
+      cell.configure(with: UIImage(named: "hero")!)
+      return cell
     }
-    cell.configure(with: UIImage(named: "hero")!)
-    return cell
   }
 }
